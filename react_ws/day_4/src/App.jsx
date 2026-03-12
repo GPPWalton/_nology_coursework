e072e47a6f93c20d3bcfeffcb65d42bf96afbfb0
Gabriel Walton <gabrielppwalton@gmail.com>
2026-03-12 15:40:37 +0000

    feat(day_4): dynamically generated component content added to Header
    feat(day_4): function call moved outside returned html for cleanliness

--- react_ws/day_4/src/App.jsx
+++ react_ws/day_4/src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
 
+const getRandomWord = wordList => {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+};
 function App () {
+    const keyWords = ["Crucial", "Fundamental", "Core"];
+    const keyword = getRandomWord(keyWords);
     return (
         <section>
-            <Header />
+            <Header keyword={keyword} />
             <main>
                 <h2>Time to get started!</h2>
             </main>
--- react_ws/day_4/src/components/Header.jsx
+++ react_ws/day_4/src/components/Header.jsx
@@ -1,10 +1,10 @@
-const Header = () => {
+const Header = ({ keyword }) => {
     return (
         <header>
             <img src='/assets/react-core-concepts.png' alt='Stylized atom' />
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you
+                {keyword} React concepts you will need for almost any app you
                 are going to build!
             </p>
         </header>
