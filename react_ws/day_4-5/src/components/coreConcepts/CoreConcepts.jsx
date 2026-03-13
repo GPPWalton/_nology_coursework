85b671414aca2c4310d8e7d3a4096b6bad069dca
Gabriel Walton <gabrielppwalton@gmail.com>
2026-03-13 11:35:50 +0000

    feat(day_5): css reorded for clarity, not perfect though

--- react_ws/day_4-5/src/components/coreConcepts/CoreConcepts.jsx
+++ react_ws/day_4-5/src/components/coreConcepts/CoreConcepts.jsx
@@ -1,6 +1,7 @@
+import style from "./CoreConcepts.module.css";
 const CoreConcepts = ({ title, description, img, alt }) => {
     return (
-        <section alt={alt}>
+        <section className={style.coreConcepts} alt={alt}>
             <img src={img} />
             <h3>{title}</h3>
             <p>{description}</p>
