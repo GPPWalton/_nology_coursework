130b57763cfb217536cae0524918f7a4860008ba
Gabriel Walton <gabrielppwalton@gmail.com>
2026-03-13 10:36:26 +0000

    chore(day_5): renamed day_4 folder day_4-5, might not stay that way

--- react_ws/day_4-5/src/App.jsx
+++ react_ws/day_4-5/src/App.jsx
@@ -1,6 +1,9 @@
 import CoreConcepts from "./components/coreConcepts/CoreConcepts";
 import Header from "./components/header/Header";
 import componentImg from "/assets/components.png";
+import jsxImg from "/assets/jsx-ui.png";
+import propsImg from "/assets/config.png";
+import stateImg from "/assets/state-mgmt.png";
 
 const getRandomWord = wordList => {
     return wordList[Math.floor(Math.random() * wordList.length)];
@@ -11,9 +14,31 @@ function App () {
     const coreConcepts = [
         {
             title: "Components",
-            description: "The UI building block.",
+            description:
+                "The core UI building block - compose the user interface by combining multiple components.",
             alt: "component icon",
             img: componentImg
+        },
+        {
+            title: "JSX",
+            description:
+                "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+            alt: "jsx icon",
+            img: jsxImg
+        },
+        {
+            title: "Props",
+            description:
+                "Make components configurable (and therefore reusable) by passing input data to them.",
+            alt: "props icon",
+            img: propsImg
+        },
+        {
+            title: "State",
+            description:
+                "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+            alt: "state icon",
+            img: stateImg
         }
     ];
     return (
