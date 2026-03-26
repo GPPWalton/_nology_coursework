54f2318a952dc5b5140748ad3a491c43e75e178d
Gabriel Walton <gabrielppwalton@gmail.com>
2026-03-26 16:13:25 +0000

    feat(day_14): api endpoints now read and write to the database

--- node_ws/day_13/index.js
+++ node_ws/day_13/index.js
@@ -4,7 +4,6 @@ const api = express();
 const cors = require("cors");
 const { TodoModel } = require("./models/todo.model.js");
 const mongoose = require("mongoose");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`;
 
 api.use(express.json());
@@ -15,51 +14,49 @@ api.use(
         credentials: true
     })
 );
-/////////////////////
-const testo = new TodoModel({
-    id: Date.now(),
-    text: "Test stuff",
-    isComplete: false
-});
-// above works
-mongoose.connect(uri).then(() => {
-    try {
-        console.log("Connected to database.");
-    } catch (error) {
-        console.error("An error occured: ", error);
-    }
-});
-//test save data entry
 
 //////////////////////////////
 // toDo item : { id: number, text: string, isComplete: bool }
 const todo_list = [];
-api.get("/todo-list", (req, res) => {
-    res.send(todo_list);
+api.get("/todo-list", async (req, res) => {
+    const list = await TodoModel.find({}).exec();
+    console.log(list);
+    res.send(list);
 });
 
-api.post("/todo-list", (req, res) => {
+api.post("/todo-list", async (req, res) => {
     const task = req.body.task;
 
-    todo_list.push(task);
+    await TodoModel.create(task);
     res.send(`New task "${task.text}" added to todo-list`);
 });
 
-api.delete("/todo-list/:id", (req, res) => {
+api.delete("/todo-list/:id", async (req, res) => {
     const id = req.params.id;
-    const index = todo_list.findIndex(object => object.id === id);
-    if (index < 0 || index > todo_list.length - 1) {
-        res.send("Index of element is invalid");
+    try {
+        await TodoModel.deleteOne({ id: id }).exec();
+    } catch (error) {
+        res.send("Id of element is invalid", error);
     }
     todo_list.splice(index, 1);
     res.send(`item at ${id} has been deleted`);
 });
 
-api.delete("/todo-list/clear", (req, res) => {
-    todo_list.splice(0, todo_list.length);
-    res.send("Todo list cleared from the server");
+api.delete("/todo-list/clear", async (req, res) => {
+    try {
+        await TodoModel.deleteMany().exec();
+    } catch (error) {
+        res.send("Todo list cleared from the server");
+    }
 });
 
-api.listen(3000, () => {
-    console.log("Todo-list API running on port 3000");
+mongoose.connect(uri).then(() => {
+    try {
+        console.log("Connected to database.");
+        api.listen(3000, () => {
+            console.log("Todo-list API running on port 3000");
+        });
+    } catch (error) {
+        console.error("An error occured: ", error);
+    }
 });
